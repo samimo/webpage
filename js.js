c85c3a42f9008af78615fbdcbf04bc634cba19a8
samimo <gary870207@gmail.com>
2021-07-23 07:59:51 +0800

    7.23

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20){
+        if(this.scrollY > 2){
             $('.navbar').addClass('sticky');
         }else{
-            
+            $('.navbar').removeClass('sticky')
         }
+    });
+    //toggle menu
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
     })
 })
